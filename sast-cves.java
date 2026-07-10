0be27e3d9b6538c18274592aeae7beccdfcfe215
Akash Dhawale <235484553+cx-akash-dhawale@users.noreply.github.com>
2026-07-10 15:28:03 +0530

    Update sast-cves.java

--- sast-cves.java
+++ sast-cves.java
@@ -1,6 +1,6 @@
 public class LoginValidator extends HttpServlet {
 
-//pr17
+//pr18
 protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 throws ServletException, IOException {
 
